a4eb61d76f0f397f49f847cb29b6869f5c29d030
Lucas Cruz <124691704+ludvikcruz@users.noreply.github.com>
2024-03-01 15:29:09 +0000

    erherh

--- Voidwomb/Voidwomb/settings.py
+++ Voidwomb/Voidwomb/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # Ensure the path here is correct relative to your BASE_DIR
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

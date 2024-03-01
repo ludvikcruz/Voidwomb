1dfa38b03066e3b7d1602a2f491562439dbe6f61
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-03-01 15:33:31 +0000

    cvpemf

--- Voidwomb/Voidwomb/settings.py
+++ Voidwomb/Voidwomb/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,3 +141,9 @@ COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
 
 
 STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+
+
+
+#media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

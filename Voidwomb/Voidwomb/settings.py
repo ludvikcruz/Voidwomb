a9340a00e15dc6c5fb9e64095ba56cc14f54cf43
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-03-01 15:26:55 +0000

    .

--- Voidwomb/Voidwomb/settings.py
+++ Voidwomb/Voidwomb/settings.py
@@ -126,8 +126,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-COMPRESS_ROOT = BASE_DIR / 'static'
-
+COMPRESS_ROOT = BASE_DIR / 'static/css/'
 
 
 COMPRESS_ENABLED = True

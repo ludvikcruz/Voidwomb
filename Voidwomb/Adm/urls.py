0fcd0d48d62853fc45b5aed469265bd6cde9031f
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2024-03-06 17:35:48 +0000

    ..

--- Voidwomb/Adm/urls.py
+++ Voidwomb/Adm/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('produto/adicionar/', views.adicionar_produto, name='adicionar_produto'),
+    path('produto/editar/<int:id>/', views.editar_produto, name='editar_produto'),
+    path('produto/eliminar/<int:id>/', views.eliminar_produto, name='eliminar_produto'),
+    path('produtos/', views.lista_produtos, name='lista_produtos'),
 ]
--- Voidwomb/Adm/views.py
+++ Voidwomb/Adm/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Produto
+from .forms import ProdutoForm  # Supondo que você tenha um formulário Django para o produto
 
-# Create your views here.
+def lista_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'SeuApp/lista_produtos.html', {'produtos': produtos})
+
+def adicionar_produto(request):
+    if request.method == "POST":
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm()
+    return render(request, 'SeuApp/form_produto.html', {'form': form})
+
+def editar_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == "POST":
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'SeuApp/form_produto.html', {'form': form})
+
+def eliminar_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == "POST":

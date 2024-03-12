8e546ef185ef74d119705de4b1422a9efe493619
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2024-03-12 16:42:03 +0000

    ,

--- Adm/urls.py
+++ Adm/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('upload-excel/', views.upload_excel_view, name='upload_excel'),
     path('users/login',views.login_view,name ='login'),
     path('users/register',views.register_view,name ='register'),
-    path('users/logout',views.register_view,name ='logout'),
+    path('users/logout',views.logout_view,name ='logout'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

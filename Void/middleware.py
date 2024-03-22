6f692a45f9d53da0cc4b2122295e867335d18c22
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-03-22 15:38:51 +0000

    s

--- Void/middleware.py
+++ Void/middleware.py
@@ -11,3 +11,13 @@ class Custom404Middleware:
         if response.status_code == 404:
             return HttpResponseNotFound(render_to_string('404.html', request=request))
         return response
+
+class Custom500Middleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if response.status_code == 500:
+            return HttpResponseNotFound(render_to_string('500.html', request=request))
+        return response

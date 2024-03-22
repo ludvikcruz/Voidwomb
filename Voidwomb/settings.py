5b894f9ce6cdc21c2b614e93e6e0badcbc971e83
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-03-22 15:16:24 +0000

    s

--- Voidwomb/settings.py
+++ Voidwomb/settings.py
@@ -44,7 +44,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3vndy%grhk*7v(et3lpwn1+0_-)776klg0@6vx)#2krt==^@z9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']

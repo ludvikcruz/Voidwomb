a8778dfc5d2c3ebb8a67327b5915aefca2cbd824
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-03-22 12:18:52 +0000

    .

--- Voidwomb/settings.py
+++ Voidwomb/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'Voidwomb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Adm/templates'), os.path.join(BASE_DIR, 'Void/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
